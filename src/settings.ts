134a77439f2d581ce5726973914f944962319983
Blake Tourneur <bla.tourneur@stjohns.sa.edu.au>
2022-01-25 08:59:21 +1030

    Fix naming error

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'SpaNETPlatform';
+export const PLATFORM_NAME = 'SpaNETPlugin';
 
 /**
  * This must match the name of your plugin as defined the package.json
